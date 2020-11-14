6eb68afd2ca0c58bed83676c8b8efe6753329eb0
Rodrigo Muniz <rcmuniz51@gmail.com>
2020-11-13 21:54:21 -0300

    Updates

---
+++ precache-manifest.3d9320022320cce82840bac823843910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d352c01467758d19d2b90ff0e0c95d",
+    "url": "/octo-tasks/index.html"
+  },
+  {
+    "revision": "7fc827c25aa85a2a16a3",
+    "url": "/octo-tasks/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "238dddc76e16762714e7",
+    "url": "/octo-tasks/static/css/main.1fd9c8b6.chunk.css"
+  },
+  {
+    "revision": "7fc827c25aa85a2a16a3",
+    "url": "/octo-tasks/static/js/2.7fcf8608.chunk.js"
+  },
+  {
+    "revision": "04b164ef0a1b6f149a4da8a5be55bb64",
+    "url": "/octo-tasks/static/js/2.7fcf8608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "238dddc76e16762714e7",
+    "url": "/octo-tasks/static/js/main.919e0520.chunk.js"
+  },
+  {
+    "revision": "b3d708c194382b118b44",
+    "url": "/octo-tasks/static/js/runtime-main.7f3b30e3.js"
+  }
+]);
